e4e29c0546e3d65fe7e23f5b949847522733de43
gtara <guoda.t@gmail.com>
2015-11-30 08:22:41 +0000

    some initial work on angular routing

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,3 +1,10 @@
-var cooltoolsApp = angular.module('cooltools', []);
+var cooltoolsApp = angular.module('cooltools', ["ngRoute"]);
 
-
+cooltoolsApp.config(function($routeProvider) {
+	$routeProvider.when("/requests", {
+		templateUrl : "/views/requests.html"
+	});
+	$routeProvider.when("/review", {
+		templateUrl : "/views/review.html"
+	});
+});
